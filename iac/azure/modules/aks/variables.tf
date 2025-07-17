05d567537de3071c0a6a9559a0e0bd8f95c94964
neeraj kr <neeraj.kumar@digital-dolphin.com>
2025-07-17 22:07:02 +0400

    update k8 version again

--- iac/azure/modules/aks/variables.tf
+++ iac/azure/modules/aks/variables.tf
@@ -21,7 +21,7 @@ variable "dns_prefix" {
 variable "kubernetes_version" {
   description = "Kubernetes version"
   type        = string
-  default     = "1.21"
+  default     = "1.27"
 }
 
 variable "node_count" {
