b916b59e5dfba788ba792dbf50c7e05d7adc23c2
neeraj kr <neeraj.kumar@digital-dolphin.com>
2025-07-17 21:12:32 +0400

    added subnets

--- iac/azure/modules/vnet/main.tf
+++ iac/azure/modules/vnet/main.tf
@@ -4,3 +4,17 @@ resource "azurerm_virtual_network" "this" {
   resource_group_name = var.resource_group_name
   address_space       = var.address_space
 }
+
+resource "azurerm_subnet" "dev-web-subnet" {
+  name          = "dev-web-subnet"
+  resource_group_name = var.resource_group_name
+  virtual_network_name = var.vnet_name
+  address_prefixes = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "dev-app-subnet" {
+  name = "dev-app-subnet"
+  resource_group_name = var.resource_group_name
+  virtual_network_name = var.vnet_name
+  address_prefixes = ["10.0.2.0/24"]
+}
--- iac/azure/modules/vnet/output.tf
+++ iac/azure/modules/vnet/output.tf
@@ -1,3 +1,11 @@
 output "vnet_id" {
   value = azurerm_virtual_network.this.id
 }
+
+output "dev_web_subnet_id" {
+  value = azurerm_subnet.dev-web-subnet.id
+}
+
+output "dev_app_subnet_id" {
+  value = azurerm_subnet.dev-app-subnet.id
+}
